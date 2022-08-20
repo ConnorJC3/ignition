b961e92b72b613a5d7510db102633cb4508d68b0
ConnorJC <github@connorjc.io>
2022-08-20 14:23:54 -0400

    ESXi config

--- convert.sh
+++ convert.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env bash
 
-docker run --interactive --rm quay.io/coreos/butane:release --strict < topeka.bu > topeka.ign
+docker run --interactive --rm quay.io/coreos/butane:release --strict < config.bu | base64 -w 0
